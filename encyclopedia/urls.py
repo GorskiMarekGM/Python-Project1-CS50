13dd651dd4181cf196e924d9565fa2fcb830705f
marekgorskiwsb@gmail.com <marekgorskiwsb@gmail.com>
2020-09-27 08:51:09 +0200

    Added create

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name="encyclopedia"
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.get_content, name="get_content"),
-    path("search/", views.search, name="search")
+    path("search/", views.search, name="search"),
+    path("create/", views.create, name="create"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from . import util
 
+class CreateEntityForm(forms.Form):
+    title = forms.CharField(label = "Title")
+    content = forms.CharField(widget=forms.Textarea, label="Content")
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -27,4 +33,20 @@ def search(request):
 
     return render(request, "encyclopedia/search.html",{
         "result_list": result_list
+    })
+
+def create(request):
+    if request.method == "POST":
+        form = CreateEntityForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            content = form.cleaned_data["content"]
+            util.save_entry(title,content)
+        else:
+            return render(request,"ecnyclopecia/create.html",{
+                "form":form
+            })
+
+    return render(request, "encyclopedia/create.html",{
+        "form": CreateEntityForm()
     })
